8e3a3ba4cda7bc5aa9fc41e427632179003443a3
koreaygj <ygj1828@gmail.com>
2023-10-20 00:48:42 +0900

    feat:contents slider with button

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,7 +1,9 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import styles from "./css/Slider.module.css";
 import PropTypes from "prop-types";
 import ContentContainer from "./ContentContainer";
+import leftButtonImage from "../images/leftBtn.png";
+import rightButtonImage from "../images/rightBtn.png";
 
 function Slider({
   title,
@@ -12,6 +14,7 @@ function Slider({
   listInfo,
   sortBy,
 }) {
+  const [slide, setSlide] = useState(0);
   const [loading, setLoading] = useState(true);
   const [contents, setContents] = useState([]);
   const ACCESS_TOKEN = process.env.REACT_APP_MOVIE_TOKEN;
@@ -30,7 +33,7 @@ function Slider({
       return `${BaseURL}${type}/${listInfo}?language=${language}&page=1`;
     }
   };
-  const getContents = useCallback(async () => {
+  const getContents = async () => {
     const URL = getURL();
     const json = await (
       await fetch(URL, option).catch((error) => {
@@ -38,23 +41,57 @@ function Slider({
       })
     ).json();
     setContents(() => json.results);
-    console.log(contents);
     setLoading(false);
-  }, [setContents, contents, getURL, option]);
+  };
+
+  const goPrev = () => {
+    if (slide < 0) setSlide((cur) => cur + 40);
+    console.log(slide);
+  };
+
+  const goNext = () => {
+    if (slide >= -300) setSlide((cur) => cur - 40);
+    console.log(slide);
+  };
+
+  const isLeftEnd = () => {
+    return slide <= 0 ? true : false;
+  };
+
+  const isRightEnd = () => {
+    return slide >= 300 ? true : false;
+  };
 
   useEffect(() => {
     getContents();
   }, []);
   return (
     <div className={styles.contentsSliderSection}>
-      <h2 className={styles.contentsSliderTitle}>{title}</h2>
-      <div className={styles.slide}>
+      <p className={styles.contentsSliderTitle}>{title}</p>
+      <div
+        className={styles.leftBtn}
+        onClick={goPrev}
+        style={{ display: isLeftEnd() ? "" : "" }}
+      >
+        <img src={leftButtonImage} />
+      </div>
+      <div
+        className={styles.rightBtn}
+        onClick={goNext}
+        style={{ display: isRightEnd() ? "" : "" }}
+      >
+        <img src={rightButtonImage} />
+      </div>
+      <div
+        className={styles.slide}
+        style={{ transform: `translateX(${slide}%)` }}
+      >
         {loading ? (
           <h1>Loading...</h1>
         ) : (
-          contents.map((content) => {
+          contents.map((content, index) => {
             return (
-              <div>
+              <div contentId={index}>
                 <ContentContainer
                   type={type}
                   id={content.id}
