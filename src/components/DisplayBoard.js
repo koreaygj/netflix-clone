bd1654c52da5b5e268ee6d09e6b7cd2f6d1244c0
koreaygj <ygj1828@gmail.com>
2023-10-20 15:43:58 +0900

    feat:main-poster fade-in fade-out

--- src/components/DisplayBoard.js
+++ src/components/DisplayBoard.js
@@ -3,8 +3,8 @@ import styles from "./css/DisplayBoard.module.css";
 
 function DisplayBoard({ id }) {
   const [toPlay, setToPlay] = useState(false);
-  const [videos, SetVideos] = useState([]);
-  const [youtubeKey, SetYoutubeKey] = useState("");
+  const [postImg, setPostImg] = useState("");
+  const [youtubeKey, setYoutubeKey] = useState("");
   const ACCESS_TOKEN = process.env.REACT_APP_MOVIE_TOKEN;
   const BaseURL = "https://api.themoviedb.org/3/";
   const option = {
@@ -14,39 +14,61 @@ function DisplayBoard({ id }) {
       Authorization: `Bearer ${ACCESS_TOKEN}`,
     },
   };
-  const getRecommendMovies = useCallback(async () => {
-    const URL = `${BaseURL}tv/${id}/videos?include_video_language&language=ko`;
+
+  const getPoster = async () => {
+    const URL = `${BaseURL}tv/${id}?language=ko`;
+    const json = await (
+      await fetch(URL, option).catch((error) => {
+        console.log(`error message!!!! : ${error}`);
+      })
+    ).json();
+    setPostImg(() => json.backdrop_path);
+  };
+
+  const getVideo = async () => {
+    const URL = `${BaseURL}tv/${id}/videos?language=ko`;
     const json = await (
       await fetch(URL, option).catch((error) => {
         console.log(error);
       })
     ).json();
+    getVideoKey(json);
+  };
 
-    console.log(json);
-    SetVideos(() => json.results);
-    if (json) getVideoKey(json);
-  }, [videos]);
   const getVideoKey = (json) => {
     json.results.map((video) => {
-      if (video.type === "Teaser") SetYoutubeKey(() => video.key);
+      if (video.type === "Teaser") setYoutubeKey(() => video.key);
     });
   };
+
   const playVideo = () => {
     setToPlay((prev) => !prev);
+    console.log(toPlay);
   };
   useEffect(() => {
-    getRecommendMovies();
+    getPoster();
+    getVideo();
   }, []);
   return (
-    <div className={styles.area}>
-      <iframe
-        className={styles.video}
-        src={`https://www.youtube.com/embed/${youtubeKey}?autoplay=1&mute=1&controls=0`}
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-      ></iframe>
+    <div>
+      <div
+        className={styles.area}
+        onMouseEnter={() => playVideo()}
+        onMouseLeave={() => playVideo()}
+      >
+        <img
+          src={`https://image.tmdb.org/t/p/original${postImg}`}
+          className={toPlay ? styles.posterOff : styles.posterOn}
+        />
+        <iframe
+          className={styles.videoOn}
+          src={`https://www.youtube.com/embed/${youtubeKey}?autoplay=1&mute=1&controls=0&rel=0&modestbranding=1`}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
   );
 }
